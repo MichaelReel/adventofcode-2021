f73659001f6b515f56f5160352dda3ce5485349e
Michael Reel <mykilr@gmail.com>
2021-12-01 08:53:11 +0000

    Failing part_01 a

--- Day_01/day_01.py
+++ Day_01/day_01.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
+from itertools import filterfalse
 
-# If we need to read input, uncomment:
-# input_file = open("Day_01/input", "r")
-# lines = [x for x in input_file.readlines()]
+
+input_file = open("Day_01/input", "r")
+lines = [x.strip() for x in input_file.readlines()]
+# lines = [
+#     199,
+#     200,
+#     208,
+#     210,
+#     200,
+#     207,
+#     240,
+#     269,
+#     260,
+#     263,
+# ]
 
 # Part 1
 print("Part 1:")
 
-part_01 = "not implemented yet"
+part_01 = 0
+for i, item in enumerate(lines[:-1]):
+    item_1 = lines[i + 1]
+    part_01 += 1 if item_1 > item else 0
+    print(f"{item} <? {item_1}: {part_01}")
+
 print(f"Result: {part_01}")
 
 # Part 2
